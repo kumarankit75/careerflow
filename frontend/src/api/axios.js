e0dc9e012a1409d94ffc50156f11e6dd3a5ed507
kumarankit75 <kumarankitgovindchak@gmail.com>
2026-01-31 13:57:59 +0530

    Update frontend API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -10,7 +10,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://careerflow-backend-oy7i.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
